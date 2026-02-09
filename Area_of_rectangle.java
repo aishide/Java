851306b63a14b26865dcadc96b6600ab1ea27516
Aishi De <149359864+aishide@users.noreply.github.com>
2026-02-09 14:37:15 +0530

    Add files via upload

---
+++ Area_of_rectangle.java
@@ -0,0 +1,14 @@
+public class Area_of_rectangle {
+    public static void main (String args[]) {
+
+        int length = 24;
+        int breadth = 12;
+
+        int area = length * breadth;
+
+        System.out.println("Area of reactangle is length x breadth");
+        System.out.println("Length of rectangle is : " + length);
+        System.out.println("Breadth of rectangle is : "+ breadth);
+        System.out.println("Area of rectangle is : " + area);
+    }
+}
