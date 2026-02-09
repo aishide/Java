d871843aa4a21f386084ff8a7d46a879d294632d
Aishi De <149359864+aishide@users.noreply.github.com>
2026-02-09 20:14:12 +0530

    Add files via upload

---
+++ People_count.java
@@ -0,0 +1,17 @@
+public class People_count { 
+    public static void main (String args[]) {
+        System.out.println("This is a demo basic program which counts the number of people in the room.");
+        int people = 0;
+        people++;
+        people++;
+        System.out.println("Current people inside the room : " + people);
+        people++;
+        people--;
+        people--;
+        System.out.println("Current people inside the room : " + people);
+        people++;
+        people++;
+        System.out.println("Current people inside the room : " + people);
+
+    }
+}
